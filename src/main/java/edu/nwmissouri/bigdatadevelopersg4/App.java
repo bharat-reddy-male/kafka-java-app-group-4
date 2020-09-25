493c0cee4e8670c33a5c65a01f5c1945ed8c76ab
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-09-24 21:00:21 -0500

    add maven archtype

---
+++ src/main/java/edu/nwmissouri/bigdatadevelopersg4/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdatadevelopersg4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "We are Group - 4" );
+    }
+}
